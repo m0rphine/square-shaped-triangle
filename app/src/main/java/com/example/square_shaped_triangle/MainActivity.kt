b377719fd077ef1a1ed29f2227587a0db148a39d
Iryna Piatrova <irina_petrova759@mail.ru>
2019-11-20 14:39:56 +0300

    Create application

---
+++ app/src/main/java/com/example/square_shaped_triangle/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.square_shaped_triangle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
