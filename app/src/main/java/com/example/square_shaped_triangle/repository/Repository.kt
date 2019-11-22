b7a7e7c31cb35aea443fcbe8f95610c01f469b8d
Maria Kirdun <manya.gumil@gmail.com>
2019-11-22 22:38:45 +0300

    add Database and Room

--- app/src/main/java/com/example/square_shaped_triangle/repository/Repository.kt
+++ app/src/main/java/com/example/square_shaped_triangle/repository/Repository.kt
@@ -1,2 +1,17 @@
 package com.example.square_shaped_triangle.repository
 
+import androidx.lifecycle.LiveData
+import com.example.square_shaped_triangle.database.AppDatabase
+import com.example.square_shaped_triangle.database.Event
+import com.example.square_shaped_triangle.database.Game
+import com.example.square_shaped_triangle.database.User
+
+class Repository (private  val database: AppDatabase) {
+
+    val users: LiveData<List<User>> = database.dao.getUsers()
+
+    val games: LiveData<List<Game>> = database.dao.getGames()
+
+    val events: LiveData<List<Event>> = database.dao.getEvent()
+
+}
---
+++ app/src/main/java/com/example/square_shaped_triangle/ui/GameDetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.square_shaped_triangle.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.square_shaped_triangle.R
+
+class GameDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.fragment_game_info)
+    }
+}
