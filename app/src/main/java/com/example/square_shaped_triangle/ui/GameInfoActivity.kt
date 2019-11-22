aaf0e1b99317f6ff9cf9104ae4a261681e82a427
Anna-Maria-Y <annamaria2262@gmail.com>
2019-11-23 00:56:21 +0300

    add EventInfoActivity and GameInfoActivity add strings to string add buttons to activity_event_info

---
+++ app/src/main/java/com/example/square_shaped_triangle/ui/GameInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.square_shaped_triangle.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.square_shaped_triangle.R
+
+class GameInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_info)
+    }
+}
