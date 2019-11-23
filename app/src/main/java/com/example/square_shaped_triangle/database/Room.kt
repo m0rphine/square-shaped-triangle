b87d15979bfa9371ce7aab42fe50d1648165b1df
Iryna Piatrova <irina_petrova759@mail.ru>
2019-11-23 04:59:32 +0300

    Update Repository and Room

--- app/src/main/java/com/example/square_shaped_triangle/database/Room.kt
+++ app/src/main/java/com/example/square_shaped_triangle/database/Room.kt
@@ -11,6 +11,14 @@ interface AppDao {
     @Transaction
     fun getUsers(): LiveData<List<User>>
 
+    @Query("select * from user u where u.id = :id")
+    @Transaction
+    fun getFavoriteGames(id: String): LiveData<FavoriteGames>
+
+    @Query("select * from user u where u.id = :id")
+    @Transaction
+    fun getOwnedGames(id: String): LiveData<OwnedGames>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertUser(user: User)
 
@@ -18,10 +26,25 @@ interface AppDao {
     fun getGames(): LiveData<List<Game>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertUser(game: Game)
+    fun insertGame(game: Game)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFavoriteGame(favoriteGames: FavoriteGames)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOwnedGame(ownedGames: OwnedGames)
 
     @Query("select * from event")
     fun getEvent(): LiveData<List<Event>>
+
+    @Query("select * from event ev where ev.id = :eventId")
+    fun getPlayers(eventId: String): LiveData<Players>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPlayers(players: Players)
+
+    @Query("select * from event")
+    fun getEventGames(): LiveData<List<EventGames>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertEvent(event: Event)
--- app/src/main/java/com/example/square_shaped_triangle/repository/Repository.kt
+++ app/src/main/java/com/example/square_shaped_triangle/repository/Repository.kt
@@ -1,14 +1,32 @@
 package com.example.square_shaped_triangle.repository
 
 import androidx.lifecycle.LiveData
-import com.example.square_shaped_triangle.database.AppDatabase
-import com.example.square_shaped_triangle.database.Event
-import com.example.square_shaped_triangle.database.User
+import com.example.square_shaped_triangle.database.*
 
 class Repository(private val database: AppDatabase) {
 
     val users: LiveData<List<User>> = database.dao.getUsers()
 
     val events: LiveData<List<Event>> = database.dao.getEvent()
+
+    fun favoriteGames(userId: String): LiveData<FavoriteGames> =
+        database.dao.getFavoriteGames(userId)
+
+    fun setFavoriteGame(favoriteGames: FavoriteGames) =
+        database.dao.insertFavoriteGame(favoriteGames)
+
+    fun ownedGames(userId: String): LiveData<OwnedGames> =
+        database.dao.getOwnedGames(userId)
+
+    fun setOwnedGame(ownedGames: OwnedGames) = database.dao.insertOwnedGame(ownedGames)
+
+    fun getPlayers(eventId: String): LiveData<Players> =
+        database.dao.getPlayers(eventId)
+
+    fun setPlayer(players: Players) = database.dao.insertPlayers(players)
+
+    fun setEvent(event: Event) = database.dao.insertEvent(event)
+
+    fun setUser(user: User) = database.dao.insertUser(user)
 
 }
